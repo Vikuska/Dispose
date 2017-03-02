915033429ccd2980a579088f85dc0733f55a8ee7
Viktoriya Mikhailova <viktoriya.mikh@namecheap.com>
2017-03-02 18:45:56 +0200

    sql

---
+++ sql/sql/DatabaseState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql
+{
+    public class DatabaseState : IDisposable
+    {
+        private SqlConnection _connection;
+
+        public string GetDate()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqlConnection("Server=(local);Database=master;Integrated Security=SSPI;Max pool size=215;Connection Timeout=2;");
+                _connection.Open();
+            }
+
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "SELECT getdate()";
+                return command.ExecuteScalar().ToString();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            var action = disposing ? "Disposing" : "Finalizing";
+            Console.WriteLine(string.Format("{0}, SqlConnection: {1}", action, _connection.GetHashCode().ToString()));
+            if (disposing)
+            {
+                _connection.Close();
+                _connection.Dispose();
+            }
+        }
+
+        ~DatabaseState()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ sql/sql/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                using (var state = new DatabaseState())
+                //var state = new DatabaseState();
+                {
+                    Console.WriteLine(state.GetDate().ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
